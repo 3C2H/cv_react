09ece31dd6e1bb43a7c9625b971c3b986d8f8083
3c2h <Charles_Chiacchia_student2018@wilder.school>
2021-03-16 10:02:28 +0100

    update 16/03/2021

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -27,7 +27,7 @@ class Header extends Component {
 	         <li><a className="smoothscroll" href="#resume">Formations</a></li>
             <li><a className="smoothscroll" href="#portfolio">Expérience</a></li>
             {/* <li><a className="smoothscroll" href="#testimonials">Témoignages</a></li> */}
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
          </ul>
 
       </nav>
